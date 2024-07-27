3c31572a33da077e289c5abe8e7aa601583b59a9
DhruvLohar <dhruvlohar09@gmail.com>
2024-07-27 12:26:26 +0530

    basic blueprint using next js and shadcn

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <main className="w-full flex items-center justify-center min-h-screen flex-col">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Page Not Found.
+            </h1>
+        </main>
+    )
+}
