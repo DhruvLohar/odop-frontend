16892df20c458f1d68726b673b8e8a8a7a963e9b
DhruvLohar <dhruvlohar09@gmail.com>
2024-07-30 21:49:44 +0530

    added states and constants

---
+++ src/lib/constants/productCategories.js
@@ -0,0 +1,27 @@
+export const ProductCategories = [
+    {
+        "value": "food",
+        "label": "Food Product",
+        "description": "Food products are items that are primarily consumable and derived from agricultural or culinary processes. These include edible goods such as fruits, vegetables, nuts, spices, and other consumables that are produced for nourishment and culinary use."
+    },
+    {
+        "value": "clothing",
+        "label": "Clothing Product",
+        "description": "Clothing products encompass items made from textiles and materials designed to be worn on the body. This category includes garments such as shirts, pants, dresses, and accessories like scarves and hats, all crafted to provide both function and style."
+    },
+    {
+        "value": "artwork",
+        "label": "Artwork",
+        "description": "Artwork refers to visual creations intended primarily for aesthetic appreciation and artistic expression. This includes paintings, sculptures, digital art, and other forms of visual art that are often displayed in galleries and exhibitions to convey creative ideas and emotions."
+    },
+    {
+        "value": "handicraft",
+        "label": "Handicraft Product",
+        "description": "Handicraft products are handmade items created using traditional techniques and tools. These items are often functional or decorative, reflecting cultural heritage and artisanal skills. Examples include pottery, textiles, wooden carvings, and handcrafted jewelry."
+    },
+    {
+        "value": "other",
+        "label": "Other Products",
+        "description": "Other products include items that do not fit into the primary categories above. This category is for unique or miscellaneous items that may not align with food, clothing, artwork, or handicraft classifications but are still valuable and noteworthy."
+    }
+]
